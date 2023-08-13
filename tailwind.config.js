add08606650e91de5fa5ff983fa648bc1b6ff9eb
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-08-13 22:36:08 +0200

    background image changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/testbg5.jpg')",
+        "hero-pattern": "url('/src/assets/testbg6.png')",
       },
     },
   },
