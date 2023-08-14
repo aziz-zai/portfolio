a0c301e57075cc9016f58a83f1a168033a9b7e94
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-08-14 19:53:57 +0200

    prettier added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,8 @@
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Suspense, useEffect, useRef, useState } from 'react';
-import CanvasLoader from '../Loader';
-const Computers = ({ isMobile, rotationSpeed }) => {
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import {Canvas, useFrame} from "@react-three/fiber";
+import {Suspense, useEffect, useRef, useState} from "react";
+import CanvasLoader from "../Loader";
+const Computers = ({isMobile, rotationSpeed}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   const groupRef = useRef(); // Ref for the mesh group
 
@@ -11,24 +11,22 @@ const Computers = ({ isMobile, rotationSpeed }) => {
     groupRef.current.rotation.y += 0.003; //  adjust the rotation speed here
   });
   return (
-    <group  ref={groupRef}>
-      <hemisphereLight intensity={1} groundColor="black"/>
-      <pointLight intensity={1}/>
-      <spotLight postition={[-20, 50,10]}
-      angle={0.12}
-      penumbra={1}
-      intensity={1}
+    <group ref={groupRef}>
+      <hemisphereLight intensity={1} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight postition={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 1.25}
+        position={isMobile ? [-1, -3, 1.5] : [1, -4, 1]}
+        rotation={isMobile ? [0.01, -0.1, -0.2] : [0.01, -0.1, -0.2]}
       />
-      <primitive object={computer.scene}
-      scale={isMobile ? .7 : 1.25}
-      position={isMobile ? [-1, -3, 1.5] : [1, -4, 1]}
-      rotation={isMobile ? [.01, -0.1 , -.2]:[.01, -0.1 , -.2]}/>
     </group>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
   const [rotationSpeed, setRotationSpeed] = useState(0.005);
 
   useEffect(() => {
@@ -59,23 +57,24 @@ const ComputersCanvas = () => {
     setRotationSpeed(0.005); // Restart rotation when mouse leaves
   };
 
-  return(
-    <Canvas 
-    frameloop='always'
-    shadows
-    camera={{position:[30,6,5], fov:30}}
-    gl={{preserveDrawingBuffer:true}}>
-      <Suspense fallback={<CanvasLoader/>}>
+  return (
+    <Canvas
+      frameloop="always"
+      shadows
+      camera={{position: [30, 6, 5], fov: 30}}
+      gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} rotationSpeed={rotationSpeed}/>
+        <Computers isMobile={isMobile} rotationSpeed={rotationSpeed} />
       </Suspense>
-      <Preload all/>
+      <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas
+export default ComputersCanvas;
