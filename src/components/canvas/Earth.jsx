f99954d6ccbb8620e061dd9639c344d65e2d3e01
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-08-17 00:28:07 +0200

    trying to change the center of 3d model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,26 +1,51 @@
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
-  return <primitive object={earth.scene} scale={2.5} position={0} rotation={0} />;
+  return (
+    <mesh>
+      <hemisphereLight intensity={1} groundColor='black' />
+      <pointLight intensity={1} />
+      <spotLight position={[-40, 10, 10]} angle={0.12} penumbra={1} intensity={0.5} />
+      <primitive
+        object={earth.scene}
+        scale={1}
+        position={[1, -0.5, 1]}
+        rotation={[0.015, -0.1, -0.015]}
+      />
+    </mesh>
+  );
 };
 
 const EarthCanvas = () => {
   return (
-    <Canvas shadows frameloop='demand' gl={{ preserveDrawingBuffer: true }} camera={{}}>
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 1]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.3,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-        >
-          <Earth />
-        </OrbitControls>
+        />
+        <Earth />
+
+        <Preload all />
       </Suspense>
     </Canvas>
   );
