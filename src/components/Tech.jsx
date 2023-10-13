04628c50a574ddc2ed87cb4a212510ad1186f1a3
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-10-13 18:52:53 +0200

    tech stack animation removed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,23 +5,25 @@ import { slideIn } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <motion.div
-      variants={slideIn("down", "twing", 0.2, 1)}
-      className='py-10 rounded-lg flex flex-row flex-wrap justify-center gap-10 shadow-card bg-tertiary border border-black'
-    >
-      {technologies.map((tech, index) => (
-        <div key={index} className='tooltip cursor-pointer'>
-          <div className='w-28 h-28 rounded-full flex items-center justify-center bg-secondary shadow-card'>
-            <img
-              src={tech.icon}
-              alt={tech.name}
-              className='absolute w-20 h-auto m-auto'
-            />
+    <div>
+      <h2 className='sm:text-[45px] xs:text-[35px] text-[25px] text-secondary uppercase tracking-wider text-center'>
+        Tech Stack
+      </h2>
+      <div className='py-10 rounded-lg flex flex-row flex-wrap justify-center gap-12'>
+        {technologies.map((tech, index) => (
+          <div key={index} className='tooltip cursor-pointer'>
+            <div className='w-28 h-28 rounded-full flex items-center justify-center bg-secondary shadow-card'>
+              <img
+                src={tech.icon}
+                alt={tech.name}
+                className='absolute w-20 h-auto m-auto'
+              />
+            </div>
+            <span className='tooltiptext'>{tech.name}</span>
           </div>
-          <span className='tooltiptext'>{tech.name}</span>
-        </div>
-      ))}
-    </motion.div>
+        ))}
+      </div>
+    </div>
   );
 };
 
